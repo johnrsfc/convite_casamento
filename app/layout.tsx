cce7c6ea1762bfcf7dd146e56646ba071478f600
johnrsfc <j.fernandes-costa@outlook.com>
2026-02-10 19:01:43 -0300

    feat: add ajuste URL FIVICON para wpp

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import MusicWrapper from "./components/MusicWrapper";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://convite-casamento-indol.vercel.app"),
   title: "Larissa & Jonathan | Casamento",
   description: "Convite de casamento de Larissa & Jonathan - 18 de Abril de 2026",
   icons: {
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Larissa & Jonathan | Casamento",
     description: "18 de Abril de 2026 • Mansão O Casarão",
-    images: [{ url: "/Img01.png", width: 800, height: 800 }],
+    images: [{ url: "https://convite-casamento-indol.vercel.app/Img01.png", width: 800, height: 800 }],
     type: "website",
   },
 };
